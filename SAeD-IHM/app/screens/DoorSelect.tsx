6d517a14723a741dbcc3f0ad070eeb6b877e71f6
Arthur <arthurhenrrique1705@gmail.com>
2026-04-14 18:44:28 -0300

    Criando tela de definição de porta e adicionando fontes pesonalizadas

--- SAeD-IHM/app/screens/DoorSelect.tsx
+++ SAeD-IHM/app/screens/DoorSelect.tsx
@@ -1,11 +1,45 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
 import "../app.css";
 
 export default function DoorSelect() {
-    
-    return(
-        <>
-            <div className="w-full h-screen bg-black">
-            </div>
-        </>
-    );
+    const [port, setPort] = useState(0);
+
+    let navigate = useNavigate();
+
+    const handleSubmit = () => {
+        localStorage.setItem("port", port.toString());
+        setPort(0);
+
+        return navigate("/landing");
+    }
+
+    const handlePort = (event: any) => {
+        setPort(event.target.value);
+    }
+
+  return (
+    <>
+      <div className="w-full h-screen bg-(--terciary-color) grid place-items-center">
+        <form className="flex flex-col rouded-sm size-fit bg-(--primary-color) text-(--terciary-color)">
+          <h1 className="text-center font-bold">Informe a porta aqui:</h1>
+          <input
+            type="number"
+            placeholder="9090"
+            value={port}
+            onChange={handlePort}
+            className="bg-(--terciary-color) text-(--secondary-color) text-center"
+            required
+          />
+          <button
+            type="button"
+            onClick={handleSubmit}
+            className="bg-(--terciary-color) text-(--secondary-color) rounded-xs"
+          >
+            Conectar
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
--- SAeD-IHM/app/screens/LandingPage.tsx
+++ SAeD-IHM/app/screens/LandingPage.tsx
@@ -1,3 +1,20 @@
+import { Navigate, useNavigate } from "react-router";
+
 export default function LandingPage() {
-    
+    let navigate = useNavigate()
+
+
+    document.addEventListener("onLoad", () => {
+        const port = localStorage.getItem("port");
+        console.log(port);
+
+        if (port == null || port == "0000") {
+            navigate("/");
+        }
+    })
+
+    return(
+        <>
+        </>
+    )
 }
