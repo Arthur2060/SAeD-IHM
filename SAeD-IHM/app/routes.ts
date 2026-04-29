e61ad90dbf269a04debd83e69764883d326d73fc
Arthur <arthurhenrrique1705@gmail.com>
2026-04-29 11:03:32 -0300

    Limpando trabalho antigo

--- SAeD-IHM/app/routes.ts
+++
@@ -1,9 +0,0 @@
-import { type RouteConfig, route } from "@react-router/dev/routes";
-
-export default [
-    route("/", "screens/DoorSelect.tsx"),
-    route("/landing", "screens/LandingPage.tsx"),
-    route("/manual", "screens/ManualControl.tsx"),
-    route("/sensor", "screens/SensorView.tsx"),
-    route("/constants", "screens/ConstantsDefinition.tsx"), 
-] satisfies RouteConfig;
